516c4c3268639061037ecec3f9e80a479e535771
Malcolm Wanless <mwanless2@my.bcit.ca>
2025-01-24 00:22:38 -0800

    Done lab wk 2

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,6 @@
 # pages/urls.py
 from django.urls import path, include
-from .views import homePageView, aboutPageView, results, homePost, todos, register, message, logoutView
+from .views import homePageView, aboutPageView, results, homePost, todos, register, message, logoutView, secretArea
 
 urlpatterns = [
     path('', homePageView, name='home'),
@@ -13,4 +13,5 @@ urlpatterns = [
     path('message/<str:msg>/<str:title>/', message, name="message"),  # <-- added
     path('', include("django.contrib.auth.urls")), # <-- added
     path("logout/", logoutView, name="logout"),
+    path("secret/", secretArea, name="secret"),
 ]
